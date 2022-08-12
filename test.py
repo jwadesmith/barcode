781a565bbb45b41220989e9aebba16b97c73add6
jwadesmith <49076596+jwadesmith@users.noreply.github.com>
2022-08-12 09:55:42 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -1,25 +1,75 @@
 import csv
 import datetime
+import time
 
-sn1 = input("Scan Barcode 1" , )
-sn2 = input("Scan Barcode 2")
-sn3 = input("Scan Barcode 3")
-sn4 = input("Scan Barcode 4")
-sn5 = input("Scan Barcode 5")
-sn6 = input("Scan Barcode 6")
+def winput(title, sentence):
+    import tkinter as tk
+    from tkinter import simpledialog
+    tk.Tk().withdraw()
+    y = simpledialog.askinteger(title, sentence)
+    return y
 
+sn1 = winput("Handle Scanning Sequence", "Scan Barcode 1")
+print(sn1)
 
-with open('test.csv', mode='w') as csv_file:
-    fieldnames = ['SN', 'Time', 'Data']
+def winput(title, sentence):
+    import tkinter as tk
+    from tkinter import simpledialog
+    tk.Tk().withdraw()
+    y = simpledialog.askinteger(title, sentence)
+    return y
+sn2 = winput("Handle Scanning Sequence", "Scan Barcode 2")
+print(sn2)
+def winput(title, sentence):
+    import tkinter as tk
+    from tkinter import simpledialog
+    tk.Tk().withdraw()
+    y = simpledialog.askinteger(title, sentence)
+    return y
+sn3 = winput("Handle Scanning Sequence", "Scan Barcode 3")
+print(sn3)
+def winput(title, sentence):
+    import tkinter as tk
+    from tkinter import simpledialog
+    tk.Tk().withdraw()
+    y = simpledialog.askinteger(title, sentence)
+    return y
+sn4 = winput("Handle Scanning Sequence", "Scan Barcode 4")
+print(sn4)
+def winput(title, sentence):
+    import tkinter as tk
+    from tkinter import simpledialog
+    tk.Tk().withdraw()
+    y = simpledialog.askinteger(title, sentence)
+    return y
+sn5 = winput("Handle Scanning Sequence", "Scan Barcode 5")
+print(sn5)
+def winput(title, sentence):
+    import tkinter as tk
+    from tkinter import simpledialog
+    tk.Tk().withdraw()
+    y = simpledialog.askinteger(title, sentence)
+    return y
+sn6 = winput("Handle Scanning Sequence", "Scan Barcode 6")
+print(sn6)
+def winput(title, sentence):
+    import tkinter as tk
+    from tkinter import simpledialog
+    tk.Tk().withdraw()
+    y = simpledialog.askinteger(title, sentence)
+    return y
+
+with open('C:/Users/15754/PycharmProjects//Barcode/test.csv', mode='a') as csv_file:
+    fieldnames = ['SN', 'Time', 'Data', 'File Location']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
     writer.writeheader()
-    writer.writerow({'SN': sn1, 'Time': 'TBD', 'Data': 'TBD'})
-    writer.writerow({'SN': sn2, 'Time': 'TBD', 'Data': 'TBD'})
-    writer.writerow({'SN': sn3, 'Time': 'TBD', 'Data': 'TBD'})
-    writer.writerow({'SN': sn4, 'Time': 'TBD', 'Data': 'TBD'})
-    writer.writerow({'SN': sn5, 'Time': 'TBD', 'Data': 'TBD'})
-    writer.writerow({'SN': sn6, 'Time': 'TBD', 'Data': 'TBD'})
+    writer.writerow({'SN': sn1, 'Time': 'TBD', 'Data': 'TBD', 'File Location': 'TBD'})
+    writer.writerow({'SN': sn2, 'Time': 'TBD', 'Data': 'TBD', 'File Location': 'TBD'})
+    writer.writerow({'SN': sn3, 'Time': 'TBD', 'Data': 'TBD', 'File Location': 'TBD'})
+    writer.writerow({'SN': sn4, 'Time': 'TBD', 'Data': 'TBD', 'File Location': 'TBD'})
+    writer.writerow({'SN': sn5, 'Time': 'TBD', 'Data': 'TBD', 'File Location': 'TBD'})
+    writer.writerow({'SN': sn6, 'Time': 'TBD', 'Data': 'TBD', 'File Location': 'TBD'})
 
 
 
